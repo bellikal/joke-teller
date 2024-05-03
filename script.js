028c8d65fb0a59e2abada5697104e8d6feffb7db
bellikal <belliz@me.com>
2024-05-03 13:57:47 +0200

    Improve fetch logic, enhance error messages, and modernize syntax

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
+// Constants
+const apiUrl = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
+
+// Get DOM elements
 const jokeButton = document.getElementById('button');
+const bubbleElement = document.querySelector('.bubble');
 const speechAudio = document.getElementById('audio');
 
-// Disable/Enable Button
-function toggleButtonDisabledState() {
+// Toggle button disabled state
+const toggleButtonDisabledState = () => {
     jokeButton.disabled = !jokeButton.disabled;
-}
+};
 
-// Passing Joke to VoiceRSS API
-function speakJoke(joke) {
-    console.log('tell me:', joke);
+// Use VoiceRSS API to speak a joke
+const speakJoke = (joke) => {
     VoiceRSS.speech({
         key: 'fd73e79b4011402b97dd9a4faa304864',
         src: joke,
@@ -19,32 +23,25 @@ function speakJoke(joke) {
         f: '44khz_16bit_stereo',
         ssml: false
     });
-}
+};
 
-// Get Jokes from Joke-API
-async function fetchAndDisplayJoke() {
-    let joke = '';
-    const apiUrl = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
-    const bubbleElement = document.querySelector('.bubble');
-
+// Fetch a joke from the Joke API and display it
+const fetchAndDisplayJoke = async () => {
     try {
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        if (data.setup) {
-            joke = `${data.setup} ... ${data.delivery}`;
-        } else {
-            joke = data.joke;
-        }
+        const { setup, delivery, joke } = await response.json();
+        const finalJoke = setup ? `${setup} ... ${delivery}` : joke;
+
         // Displaying joke in the Speech-Bubble
-        bubbleElement.textContent = joke;
+        bubbleElement.textContent = finalJoke;
         // Text-to-Speech
-        speakJoke(joke);
+        speakJoke(finalJoke);
         // Disable Button
         toggleButtonDisabledState();
     } catch (error) {
         // Catch Errors Here
         console.log('Error fetching joke:', error);
-        bubbleElement.textContent = "Failed to fetch joke.";
+        bubbleElement.textContent = "Sorry, the joke could not be loaded. Please try again!";
     }
 }
 
